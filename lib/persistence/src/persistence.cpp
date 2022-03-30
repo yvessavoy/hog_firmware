4337d0aaa7ec0849fa11575c7d24a4d5e9f62dd6
Yves Savoy <10594588+yvessavoy@users.noreply.github.com>
2022-03-30 15:12:38 +0200

    Implement a abstraction layer over the EEPROM-flash to store and retrieve user data

---
+++ lib/persistence/src/persistence.cpp
@@ -0,0 +1,181 @@
+#include "persistence.hpp"
+#include <EEPROM.h>
+
+void initPersistency()
+{
+    EEPROM.begin(EEPROM_SIZE);
+}
+
+void clearPersistency()
+{
+    EEPROM.write(MIN_TEMP_ADDR, 0xff);
+    EEPROM.commit();
+}
+
+void storeMinTemp(uint8_t temp)
+{
+    EEPROM.write(MIN_TEMP_ADDR, temp);
+    EEPROM.commit();
+}
+
+void storeMaxTemp(uint8_t temp)
+{
+    EEPROM.write(MAX_TEMP_ADDR, temp);
+    EEPROM.commit();
+}
+
+void storeMinHumidity(uint8_t humidity)
+{
+    EEPROM.write(MIN_HUMIDITY_ADDR, humidity);
+    EEPROM.commit();
+}
+
+void storeMaxHumidity(uint8_t humidity)
+{
+    EEPROM.write(MAX_HUMIDITY_ADDR, humidity);
+    EEPROM.commit();
+}
+
+void storeMinLight(uint16_t light)
+{
+    EEPROM.write(MIN_LIGHT_ADDR, (light >> 8));
+    EEPROM.write(MIN_LIGHT_ADDR + 1, (light & 0xFF));
+    EEPROM.commit();
+}
+
+void storeMaxLight(uint16_t light)
+{
+    EEPROM.write(MAX_LIGHT_ADDR, (light >> 8));
+    EEPROM.write(MAX_LIGHT_ADDR + 1, (light & 0xFF));
+    EEPROM.commit();
+}
+
+void storeMinCO2(uint16_t co2)
+{
+    EEPROM.write(MIN_CO2_ADDR, (co2 >> 8));
+    EEPROM.write(MIN_CO2_ADDR + 1, (co2 & 0xFF));
+    EEPROM.commit();
+}
+
+void storeMaxCO2(uint16_t co2)
+{
+    EEPROM.write(MAX_CO2_ADDR, (co2 >> 8));
+    EEPROM.write(MAX_CO2_ADDR + 1, (co2 & 0xFF));
+    EEPROM.commit();
+}
+
+uint8_t retrieveMinTemp()
+{
+    return EEPROM.read(MIN_TEMP_ADDR);
+}
+
+uint8_t retrieveMaxTemp()
+{
+    return EEPROM.read(MAX_TEMP_ADDR);
+}
+
+uint8_t retrieveMinHumidity()
+{
+    return EEPROM.read(MIN_HUMIDITY_ADDR);
+}
+
+uint8_t retrieveMaxHumidity()
+{
+    return EEPROM.read(MAX_HUMIDITY_ADDR);
+}
+
+uint16_t retrieveMinLight()
+{
+    uint8_t msb = EEPROM.read(MIN_LIGHT_ADDR);
+    uint8_t lsb = EEPROM.read(MIN_LIGHT_ADDR + 1);
+    return (msb << 8) + lsb;
+}
+
+uint16_t retrieveMaxLight()
+{
+    uint8_t msb = EEPROM.read(MAX_LIGHT_ADDR);
+    uint8_t lsb = EEPROM.read(MAX_LIGHT_ADDR + 1);
+    return (msb << 8) + lsb;
+}
+
+uint16_t retrieveMinCO2()
+{
+    uint8_t msb = EEPROM.read(MIN_CO2_ADDR);
+    uint8_t lsb = EEPROM.read(MIN_CO2_ADDR + 1);
+    return (msb << 8) + lsb;
+}
+
+uint16_t retrieveMaxCO2()
+{
+    uint8_t msb = EEPROM.read(MAX_CO2_ADDR);
+    uint8_t lsb = EEPROM.read(MAX_CO2_ADDR + 1);
+    return (msb << 8) + lsb;
+}
+
+void storeWlanSsid(char ssid[32])
+{
+    for (uint8_t i = 0; i < 32; i++)
+    {
+        EEPROM.write(WLAN_SSID_ADDR + i, ssid[i]);
+    }
+    EEPROM.commit();
+}
+
+void storeWlanPassword(char password[63])
+{
+    for (uint8_t i = 0; i < 63; i++)
+    {
+        EEPROM.write(WLAN_PW_ADDR + i, password[i]);
+    }
+    EEPROM.commit();
+}
+
+void retrieveWlanSsid(char *ssid)
+{
+    for (uint8_t i = 0; i < 32; i++)
+    {
+        ssid[i] = EEPROM.read(WLAN_SSID_ADDR + i);
+    }
+}
+
+void retrieveWlanPassword(char *password)
+{
+    for (uint8_t i = 0; i < 63; i++)
+    {
+        password[i] = EEPROM.read(WLAN_PW_ADDR + i);
+    }
+}
+
+void storeGrafanaUrl(char url[255])
+{
+    for (uint8_t i = 0; i < 255; i++)
+    {
+        EEPROM.write(GRAFANA_URL_ADDR + i, url[i]);
+    }
+    EEPROM.commit();
+}
+
+void retrieveGrafanaUrl(char *url)
+{
+    for (uint8_t i = 0; i < 255; i++)
+    {
+        url[i] = EEPROM.read(GRAFANA_URL_ADDR + i);
+    }
+}
+
+void storeTimer(uint8_t minutes, uint8_t seconds)
+{
+    EEPROM.write(TIMER_MIN_ADDR, minutes);
+    EEPROM.write(TIMER_SEC_ADDR, seconds);
+    EEPROM.commit();
+}
+
+uint8_t retrieveTimerMinutes()
+{
+    return EEPROM.read(TIMER_MIN_ADDR);
+}
+
+uint8_t retrieveTimerSeconds()
+{
+    return EEPROM.read(TIMER_SEC_ADDR);
+}
---
+++ lib/persistence/src/persistence.hpp
@@ -0,0 +1,53 @@
+#ifndef _PERSISTENCE_H
+#define _PERSISTENCE_H
+
+#include <stdint.h>
+
+#define EEPROM_SIZE 369
+#define WLAN_SSID_ADDR 0
+#define WLAN_PW_ADDR 33
+#define MIN_TEMP_ADDR 99
+#define MAX_TEMP_ADDR 100
+#define MIN_HUMIDITY_ADDR 101
+#define MAX_HUMIDITY_ADDR 102
+#define MIN_LIGHT_ADDR 103
+#define MAX_LIGHT_ADDR 105
+#define MIN_CO2_ADDR 107
+#define MAX_CO2_ADDR 109
+#define GRAFANA_URL_ADDR 111
+#define TIMER_MIN_ADDR 367
+#define TIMER_SEC_ADDR 368
+
+void initPersistency();
+void clearPersistency();
+
+void storeMinTemp(uint8_t temp);
+void storeMaxTemp(uint8_t temp);
+void storeMinHumidity(uint8_t humidity);
+void storeMaxHumidity(uint8_t humidity);
+void storeMinLight(uint16_t light);
+void storeMaxLight(uint16_t light);
+void storeMinCO2(uint16_t co2);
+void storeMaxCO2(uint16_t co2);
+uint8_t retrieveMinTemp();
+uint8_t retrieveMaxTemp();
+uint8_t retrieveMinHumidity();
+uint8_t retrieveMaxHumidity();
+uint16_t retrieveMinLight();
+uint16_t retrieveMaxLight();
+uint16_t retrieveMinCO2();
+uint16_t retrieveMaxCO2();
+
+void storeWlanSsid(char ssid[32]);
+void storeWlanPassword(char password[63]);
+void retrieveWlanSsid(char *ssid);
+void retrieveWlanPassword(char *password);
+
+void storeGrafanaUrl(char url[255]);
+void retrieveGrafanaUrl(char *url);
+
+void storeTimer(uint8_t minutes, uint8_t seconds);
+uint8_t retrieveTimerMinutes();
+uint8_t retrieveTimerSeconds();
+
+#endif
